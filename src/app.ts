af84fd0fbc7b280987cef6fe6b344896d80006ad
capa777 <patrykcapanda@interia.pl>
2023-05-14 19:24:40 +0200

    basic project structure and logger

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import config from "config";
+import { log } from "./utils/logger";
+
+const app = express();
+const port = config.get('port');
+
+app.listen(port, () => {
+    log.info(`App started at http://localhost:${port}`);
+});
