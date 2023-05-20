058307efc73b44673177c1950458086f41fa741d
capa777 <patrykcapanda@interia.pl>
2023-05-20 23:21:07 +0200

    auth controller tests

---
+++ config/test.ts
@@ -0,0 +1,10 @@
+export default {
+    port: 3000,
+    logLevel: "info",
+    db_host: process.env.DB_HOST,
+    db_port: process.env.DB_PORT,
+    db_username: process.env.DB_USERNAME,
+    db_password: process.env.DB_PASSWORD,
+    db_name: process.env.DB_NAME,
+    signingKey: "179u2b971b20zn189psg12sfg179sfg1pb1",
+}
